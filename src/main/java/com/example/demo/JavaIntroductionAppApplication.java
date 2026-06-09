ec5b03f1422150298812c9eb426e7e8258d63e60
3030691 <3030691@30306-91>
2026-06-09 11:45:48 +0900

    【最初の枠組み】-Java-Introduction_APPの土台です

---
+++ src/main/java/com/example/demo/JavaIntroductionAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaIntroductionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaIntroductionAppApplication.class, args);
+	}
+
+}
