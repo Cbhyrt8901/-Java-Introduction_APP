c3a9492a48b2fdf140815f268d165327b3263753
3030691 <3030691@30306-91>
2026-06-09 15:39:20 +0900

    【ステップ7.5】2.の手順、完了しました。 伊藤より

--- src/main/java/com/example/demo/controller/TeamController.java
+++ src/main/java/com/example/demo/controller/TeamController.java
@@ -1,14 +1,22 @@
 package com.example.demo.controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.demo.service.HidaService;
+import com.example.demo.service.YamamotoService;
+
 @Controller
 public class TeamController {
+	@Autowired public YamamotoService yamamotoService;
+	@Autowired public HidaService hidaService;
+	
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("leaderMsg", "リーダーです！");
-        // TODO: ここにメンバーのServiceを後で呼ぶ
+		 model.addAttribute("yamamotoMsg", yamamotoService.getProfile());
+		 model.addAttribute("hidaMsg", hidaService.getProfile());
         return "index";
     }
 }
